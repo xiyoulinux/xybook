5b5271e333da3e9ee05a4cef631c683e823cf215
blueskyflq <blueskyflq@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-11-17 14:24:24 +0000

    修改缩近，小米，注意缩近

--- test/myfind.py
+++ test/myfind.py
@@ -2,14 +2,14 @@
 import os
 key=raw_input("Please input the key words of the Ebook's name: ")
 def find(key):
- count=0
- for line in fp.readlines():
-	if key in line:
-	   count+=1
-           print line
- if count==0:
-    print "Sorry,the book doesn't exist!"
+	count=0
+	for line in fp.readlines():
+		if key in line:
+			count+=1
+			print line
+	if count==0:
+		print "Sorry,the book doesn't exist!"
 if __name__=='__main__':
- fp=open("list.txt","r")
- find(key)
- fp.close()
+	fp=open("list.txt","r")
+	find(key)
+	fp.close()
