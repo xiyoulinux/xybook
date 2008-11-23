232ec6225dce15494f3bb33a990ef8d55cdc0599
blueskyflq <blueskyflq@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-11-23 08:53:52 +0000

    增加about

---
+++ test/about.py
@@ -0,0 +1,16 @@
+#/bin/env python
+
+from Tkinter import *
+import Pmw
+root = Tk()
+Pmw.initialise()
+Pmw.aboutversion('0.1')
+Pmw.aboutcopyright('copyright xiyoulinux group\nAll rights reserved')
+Pmw.aboutcontact(
+	'for more information visit\n'+
+	'www.xiyoulinux.cn\n'+
+	'or email to:\n'
+	'xiyoulinux@googlegroup.com\n'
+	)
+about = Pmw.AboutDialog(root,applicationname='PEMS')
+root.mainloop()
