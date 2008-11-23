686d91e22127924f17d25916613f678c890df786
blueskyflq <blueskyflq@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-11-23 09:56:52 +0000

    增加listlist 此文件用于将文件夹中的列表文件列在列表框中，为配合其使用，建议将所有的列表文件的后缀改为.list

---
+++ test/listlist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from Tkinter import *
+import os
+
+root = Tk()
+#root.option_readfile('optionDB')
+root.title('Listbox')
+list = Listbox(root, width=30,height=50)
+scroll = Scrollbar(root, command=list.yview)
+list.configure(yscrollcommand=scroll.set)
+list.pack(side=LEFT)
+scroll.pack(side=RIGHT, fill=Y)
+
+for item in os.listdir(os.getcwd()):
+	if os.path.splitext(item)[1] in ('.txt',):
+		list.insert(END,item)
+root.mainloop()
