bfe1c7763740629b85a195dbbf6471b3535a0abf
michenggang <michenggang@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-11-25 04:01:24 +0000

    格式修改 -此行及以下内容将会被忽略--
    M    myfind.py

--- test/myfind.py
+++ test/myfind.py
@@ -2,7 +2,6 @@
 import os
 key=raw_input("Please input the key words of the Ebook's name: ")
 def find(key):
-<<<<<<< .mine
 	count=0
 	for line in fp.readlines():
         	if key in line:
@@ -10,22 +9,8 @@ def find(key):
             		print line
 	if count==0:
         	print "Sorry,the book doesn't exist!"
-=======
-	count=0
-	for line in fp.readlines():
-		if key in line:
-			count+=1
-			print line
-	if count==0:
-		print "Sorry,the book doesn't exist!"
->>>>>>> .r7
 if __name__=='__main__':
-<<<<<<< .mine
-    	fp=open("list.list","r")
+        fp=open("list.list","r")
     	find(key)
     	fp.close()
-=======
-	fp=open("list.list","r")
-	find(key)
-	fp.close()
->>>>>>> .r7
+
