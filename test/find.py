e57cc7e59f5d7020ba8d4ae6196f12dc2e6bd3aa
blueskyflq <blueskyflq@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-11-30 00:52:16 +0000

    解决冲突

--- test/find.py
+++ test/find.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/python
 import os
 key="xiyou"
 def find(key):
--- test/myfind.py
+++ test/myfind.py
@@ -1,16 +1,15 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 import os
 key=raw_input("Please input the key words of the Ebook's name: ")
 def find(key):
 	count=0
 	for line in fp.readlines():
-        	if key in line:
-	    		count+=1
-            		print line
+		if key in line:
+			count+=1
+			print line,
 	if count==0:
-        	print "Sorry,the book doesn't exist!"
+		print "Sorry,the book doesn't exist!"
 if __name__=='__main__':
-        fp=open("list.list","r")
-    	find(key)
-    	fp.close()
-
+	fp=open("list.list","r")
+	find(key)
+	fp.close()
