72a05bab07214fdfcea65913019f4280bdf03356
blueskyflq <blueskyflq@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-12-08 13:12:58 +0000

    文件大小显示成功

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # Just some simple data structures for the report mode listctrl examples
 import os
-columns = ["BookName","Size","CreateDate","BookDir"]
+columns = ["电子书名称","电子书大小","电子书所在路径"]
 f = open('list.list','r')
-rows=[(os.path.splitext(os.path.basename(line))[0],'','',line)for line in f]
+rows=[(os.path.splitext(os.path.basename(line.split('        ')[0]))[0], line.split('        ')[-1],line.split('        ')[0])for line in f]
 f.close()
