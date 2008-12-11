752ef477f89bfeeee44c9b1a511809b8c5596b57
blueskyflq <blueskyflq@4797f7d0-a9c5-11dd-bf8d-3712e0689f7b>
2008-12-11 05:06:39 +0000

    修正左边列表增加或删除不能刷新的问题，增加按照文件类型增加到列表的功能，修正原始目录中若没有list.list会报错的bug

--- src/data.py
+++ src/data.py
@@ -2,6 +2,11 @@
 # Just some simple data structures for the report mode listctrl examples
 import os
 columns = ["电子书名称","电子书大小","电子书所在路径"]
-f = open('list.list','r')
-rows=[(os.path.splitext(os.path.basename(line.split('        ')[0]))[0], line.split('        ')[-1],line.split('        ')[0])for line in f]
-f.close()
+if os.path.isfile('list.list'):
+    f = open('list.list','r')
+    rows=[(os.path.splitext(os.path.basename(line.split('        ')[0]))[0], line.split('        ')[-1],line.split('        ')[0])for line in f]
+    f.close()
+else:
+    f = open('list.list','w')
+    rows=[]
+    f.close()
